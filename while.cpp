6acc4c3fb5fc003d617cc9f8d78051c15c90ad4b
stephania1809 <139251465+stephania1809@users.noreply.github.com>
2023-07-18 09:22:01 -0600

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main () {int numero ;
+cout <<"ingrese un numero";
+cin>> numero;
+while(numero<=100)
+{
+    cout<<"ingrese un numero";
+    cin>>numero;
+}
+return 0;
+}
